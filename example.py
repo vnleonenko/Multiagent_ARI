e815d4d78f89c715253048f8a078326554111211
Mpkosh <koshk.mp@gmail.com>
2025-05-19 19:11:09 +0300

    added samara res from new code

--- example.py
+++ example.py
@@ -7,13 +7,13 @@ if __name__ == '__main__':
             strains_keys  = ['H1N1', 'H3N2', 'B'], 
             infected_init = [10, 0, 0], 
             alpha         = [0.78, 0.74, 0.6], 
-            lmbd          = 0.4
+            lmbd          = 0.3
             )
 
     pool.runs_params(
-            num_runs = 1, 
-            days = [1, 50],
-            data_folder = 'synthetic_sample_4000',
+            num_runs = 30, 
+            days = [1, 250],
+            data_folder = 'sampled_200k',
             )
 
     pool.age_groups_params(
@@ -21,4 +21,4 @@ if __name__ == '__main__':
             vaccined_fraction = [0, 0, 0, 0]
             )
 
-    pool.start()
+    pool.start(with_seirb=True)
